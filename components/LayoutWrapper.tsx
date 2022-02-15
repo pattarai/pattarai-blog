b348e8dab18c0bcd440d51329375b6e1d7c1fe7e
Joshuafrankle <joshuafrankle7@gmail.com>
2022-02-15 20:57:31 +0530

    Update logo

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -18,20 +18,20 @@ const LayoutWrapper = ({ children }: Props) => {
       <div className="flex flex-col justify-between h-screen">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href="/" aria-label="Pattarai Blog">
-              <div className="flex items-center justify-between">
-                <div className="mr-5">
+            <div className="flex items-center justify-between">
+              <div className="mr-5">
+                <Link href="https://www.pattarai.in/" aria-label="Pattarai Blog">
                   <Logo width="auto" height="70px" />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+                </Link>
               </div>
-            </Link>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
           </div>
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
